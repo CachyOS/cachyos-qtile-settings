fe781e984c93aac064e6da963a0c0987b0430036
Aarrayy <68795737+aarrayy@users.noreply.github.com>
2023-10-25 16:52:17 -0300

    Add nm-applet

--- etc/skel/.config/qtile/scripts/autostart.sh
+++ etc/skel/.config/qtile/scripts/autostart.sh
@@ -4,3 +4,4 @@ feh --bg-fill $HOME/.config/qtile/Wallpaper/Skyscraper.png &
 picom --config $HOME/.config/qtile/scripts/picom.conf & 
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/bin/wired &
+nm-applet &
