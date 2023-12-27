83b3097c984c6c5c121debe09ee3dd1ebe0aabd6
Aarrayy <68795737+aarrayy@users.noreply.github.com>
2023-12-27 12:43:06 -0300

    Initiate picom daemon
    This is meant to prevent picom flooding xsession with errors such as: Xlib Ignoring invalid extension

--- etc/skel/.config/qtile/scripts/autostart.sh
+++ etc/skel/.config/qtile/scripts/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 feh --bg-fill $HOME/.config/qtile/Wallpaper/Skyscraper.png &
-picom --config $HOME/.config/qtile/scripts/picom.conf & 
+picom --daemon --config $HOME/.config/qtile/scripts/picom.conf & 
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/bin/wired &
 nm-applet &
