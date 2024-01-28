291dc2a1df6e0909c97ba2ea35d41401d02c72ce
aarrayy <aarrayyzuelo@protonmail.com>
2024-01-28 12:09:03 -0300

    Add gnome-keyring daemon

--- etc/skel/.config/qtile/scripts/autostart.sh
+++ etc/skel/.config/qtile/scripts/autostart.sh
@@ -4,4 +4,6 @@ feh --bg-fill $HOME/.config/qtile/Wallpaper/Skyscraper.png &
 picom --daemon --config $HOME/.config/qtile/scripts/picom.conf & 
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/bin/wired &
+eval $(gnome-keyring-daemon --start) 
 nm-applet &
+
