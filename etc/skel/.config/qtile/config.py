aa8bedd722172c856130a228c30908baf342555a
aarrayy <aarrayyzuelo@protonmail.com>
2024-05-19 09:10:32 -0300

    Add descriptions to some keybinds

--- etc/skel/.config/qtile/config.py
+++ etc/skel/.config/qtile/config.py
@@ -61,8 +61,8 @@ keys = [
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    Key([mod], "f", lazy.window.toggle_fullscreen()),
-    Key([mod], "v", lazy.window.toggle_floating()),
+    Key([mod], "f", lazy.window.toggle_fullscreen(), desc="Toggle focused window to fullscreen"),
+    Key([mod], "v", lazy.window.toggle_floating(), desc="Toggle focused window to floating"),
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
